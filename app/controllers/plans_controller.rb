056bdfd43e33f501603d77f9b52597e086983bbd
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2018-04-05 23:16:29 +0200

    create a new plan

---
+++ app/controllers/plans_controller.rb
@@ -0,0 +1,30 @@
+class PlansController < ApplicationController
+  before_action :set_user, only: [:index, :create, :update, :destroy]
+  def index
+    @plans = @user.plans
+  end
+
+  def new
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+    @plan.user = @user
+      if @plan.save
+        redirect_to plans_path
+      else
+        render :new
+      end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def plan_params
+    params.require(:plan).permit(:name)
+  end
+end
